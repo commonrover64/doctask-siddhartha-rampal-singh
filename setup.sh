cc34ab33b5e2cfb7dc8d56c638f3b1c684435100
commonrover64 <siddhartharampalsingh@gmail.com>
2026-08-17 23:23:03 +0530

    added condition in setup .py while making dir for watcher

--- setup.sh
+++ setup.sh
@@ -25,7 +25,7 @@ echo "-- Setting up backend --"
 cd backend
 python3 -m venv venv
 source venv/bin/activate
-mkdir watched_incoming
+mkdir -p watched_incoming
 pip install -r requirements.txt --quiet
 python -m app.migrate
 
